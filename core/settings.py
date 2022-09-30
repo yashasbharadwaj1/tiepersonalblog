4992a21eb7e75ca6060d84ae4ced7490b0bd3700
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-30 16:41:42 +0530

    changed to sqlite3 because postgresql not working on heroku

--- core/settings.py
+++ core/settings.py
@@ -86,6 +86,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+
 """
 DATABASES = {
     'default': {
@@ -109,7 +121,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 DATABASES={
    'default':{
@@ -121,7 +133,7 @@ DATABASES={
       'PORT':'5432',
    }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
