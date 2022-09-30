d21c6ba32713e28ca9ce6ec76bd0013ff64409e7
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-30 16:24:48 +0530

    trial

--- core/settings.py
+++ core/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 import django_heroku
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
-import os
+
 
 TEMPLATES = [
     {
