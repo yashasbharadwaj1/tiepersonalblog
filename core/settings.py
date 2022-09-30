1b8a54b926e7d49a96bc8d29e99f6929fe4b05c6
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-30 16:35:18 +0530

    used dotenv

--- core/settings.py
+++ core/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from dotenv import load_dotenv
+load_dotenv() 
 from pathlib import Path
 import environ
 import django_heroku
-os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
-env = environ.Env()
-environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,11 +166,11 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = str(os.getenv('EMAIL_HOST'))
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
+EMAIL_HOST_PASSWORD =str(os.getenv('EMAIL_HOST_PASSWORD'))
 
-RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
+RECIPIENT_ADDRESS =str(os.getenv('RECIPIENT_ADDRESS'))
 django_heroku.settings(locals())
