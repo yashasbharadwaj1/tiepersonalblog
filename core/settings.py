68ba8e1400f5d2b114d303ef688b1d3d85ebb01f
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-09-30 16:26:54 +0530

    t1

--- core/settings.py
+++ core/settings.py
@@ -165,7 +165,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
